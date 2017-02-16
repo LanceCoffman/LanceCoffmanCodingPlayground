6512b9040eba3ac3bb2dc5368d3427125456f88c
Lance Coffman <lanceacoffman@gmail.com>
2017-02-16 04:57:36 -0500

    initial creation

---
+++ LanceCoffmanCodingPlayground/Scripts/playground.js
@@ -0,0 +1,11 @@
+﻿/* Code built by Lance Coffman for this websie*/
+
+var jumboHeight = $('.jumbotron2').outerHeight();
+function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.bg2').css('height', jumboHeight - scrolled + 'px');
+}
+
+$(window).scroll(function (e) {
+    parallax();
+});
